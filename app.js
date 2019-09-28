574daac162619fdb5b412f6c9f4e98f9f6da4d37
Student <student@itpark.ru>
2019-09-28 10:43:01 +0300

    init object

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log('worked');
+
+const author = {
+    id: 'buzova86',
+    avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
+    verifiend: true,
+};
+
+const photo = {
+    id: 'B25tEmYoZCp',
+    author: author,
+    location: 'Китай',
+    imageUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/fb3f9a02929a5ec1bdcbeca752910a05/5E3399E7/t51.2885-15/sh0.08/e35/p640x640/70352284_156096418926859_4987078674500147991_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1',
+    videoUrl: null,
+    likes: 45456,
+};
+
+const photo = {
+    id: 'B28c65boHeP',
+    author: author,
+    location: 'Владивосток',
+    imageUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/fb3f9a02929a5ec1bdcbeca752910a05/5E3399E7/t51.2885-15/sh0.08/e35/p640x640/70352284_156096418926859_4987078674500147991_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1',
+    videoUrl: 'https://youtube.com/..',
+    likes: 5656,
+};
