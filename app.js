bb407e0eb51bf603af380ea0ce5e5d360b0cb5e0
Student <student@itpark.ru>
2019-09-28 10:57:12 +0300

    Переименовали переменную

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('worked');
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
     verifiend: true,
@@ -8,7 +8,7 @@ const author = {
 
 const photo = {
     id: 'B25tEmYoZCp',
-    author: author,
+    author: buzova,
     location: 'Китай',
     imageUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/fb3f9a02929a5ec1bdcbeca752910a05/5E3399E7/t51.2885-15/sh0.08/e35/p640x640/70352284_156096418926859_4987078674500147991_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1',
     videoUrl: null,
@@ -17,7 +17,7 @@ const photo = {
 
 const photo = {
     id: 'B28c65boHeP',
-    author: author,
+    author: buzova,
     location: 'Владивосток',
     imageUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/fb3f9a02929a5ec1bdcbeca752910a05/5E3399E7/t51.2885-15/sh0.08/e35/p640x640/70352284_156096418926859_4987078674500147991_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1',
     videoUrl: 'https://youtube.com/..',
